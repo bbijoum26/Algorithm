db6fbc32738b5d6286c59050b433d737a1519fe8
bbijoum26 <139214579+bbijoum26@users.noreply.github.com>
2024-02-01 22:46:43 +0900

    [level 0] Title: x 사이의 개수, Time: 3.81 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
@@ -0,0 +1,14 @@
+def solution(myString):
+    answer = []
+    current_length = 0
+
+    for char in myString:
+        if char == "x":
+            answer.append(current_length)
+            current_length = 0
+        else:
+            current_length += 1
+
+    answer.append(current_length)
+    
+    return answer
